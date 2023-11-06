e0f0b7f27a007d516d61de5dbfe0e9cc79321900
komron01 <47138499+komron01@users.noreply.github.com>
2023-11-06 10:13:36 +0600

    v1.0
    This is initial files to upload to the github
    Первые файлы загруженные на гитхаб

---
+++ testdata.sql
@@ -0,0 +1,58 @@
+create table testdata (
+	id INT,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	email VARCHAR(50),
+	gender VARCHAR(50),
+	ip_address VARCHAR(20)
+);
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (1, 'Carleton', 'Cully', 'ccully0@blog.com', 'Male', '8.152.187.39');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (2, 'Kacey', 'Loudiane', 'kloudiane1@census.gov', 'Female', '6.145.196.198');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (3, 'Wendall', 'Ambrosio', 'wambrosio2@jigsy.com', 'Male', '216.103.228.169');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (4, 'Andree', 'Davydkov', 'adavydkov3@economist.com', 'Female', '198.37.245.17');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (5, 'Ab', 'Elcocks', 'aelcocks4@bloglovin.com', 'Male', '248.25.135.7');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (6, 'Fannie', 'Cudworth', 'fcudworth5@hatena.ne.jp', 'Female', '175.248.253.12');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (7, 'Jenny', 'Cliffe', 'jcliffe6@people.com.cn', 'Female', '250.58.198.104');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (8, 'Torry', 'Degoe', 'tdegoe7@infoseek.co.jp', 'Bigender', '11.47.242.180');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (9, 'Mattie', 'Chadney', 'mchadney8@cnet.com', 'Male', '41.63.70.205');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (10, 'Winnie', 'Pitchers', 'wpitchers9@furl.net', 'Male', '236.170.19.101');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (11, 'Matilda', 'Alan', 'malana@walmart.com', 'Female', '20.21.88.71');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (12, 'Bennie', 'Kisar', 'bkisarb@census.gov', 'Female', '107.202.242.159');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (13, 'Sebastian', 'Osbaldstone', 'sosbaldstonec@techcrunch.com', 'Male', '83.61.197.155');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (14, 'Nigel', 'Barlace', 'nbarlaced@webeden.co.uk', 'Male', '137.209.37.154');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (15, 'Antons', 'Beeswing', 'abeeswinge@dailymotion.com', 'Male', '26.30.40.79');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (16, 'Robert', 'Drane', 'rdranef@ehow.com', 'Male', '69.202.47.159');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (17, 'Shurlock', 'Argabrite', 'sargabriteg@cnet.com', 'Non-binary', '97.188.86.109');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (18, 'Melissa', 'Heustace', 'mheustaceh@php.net', 'Female', '214.19.254.23');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (19, 'Rosalinde', 'Sloss', 'rslossi@businessinsider.com', 'Female', '88.125.137.33');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (20, 'Nancy', 'Lukianov', 'nlukianovj@wsj.com', 'Female', '91.224.152.222');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (21, 'Georgina', 'Shelf', 'gshelfk@sbwire.com', 'Female', '246.44.191.204');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (22, 'Laney', 'Gittings', 'lgittingsl@nature.com', 'Male', '252.120.220.237');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (23, 'Charmine', 'Lindwasser', 'clindwasserm@netscape.com', 'Female', '96.159.60.229');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (24, 'Prudence', 'Suddaby', 'psuddabyn@utexas.edu', 'Female', '197.233.70.14');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (25, 'Arley', 'Lovick', 'alovicko@miitbeian.gov.cn', 'Male', '237.55.46.166');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (26, 'Kristel', 'Imos', 'kimosp@europa.eu', 'Female', '112.65.178.65');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (27, 'Rhys', 'Cronkshaw', 'rcronkshawq@is.gd', 'Male', '255.54.159.116');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (28, 'Felicity', 'Matthiesen', 'fmatthiesenr@youtube.com', 'Female', '96.185.117.117');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (29, 'Cary', 'Sedgeworth', 'csedgeworths@go.com', 'Male', '249.189.89.171');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (30, 'Chane', 'Redwall', 'credwallt@ft.com', 'Male', '97.86.99.186');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (31, 'Moses', 'Janiak', 'mjaniaku@salon.com', 'Male', '131.195.96.220');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (32, 'Celene', 'Mathouse', 'cmathousev@mapy.cz', 'Female', '68.200.150.53');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (33, 'Kelsey', 'Dutnell', 'kdutnellw@typepad.com', 'Male', '104.253.128.184');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (34, 'Nichole', 'Norcutt', 'nnorcuttx@desdev.cn', 'Female', '132.15.174.185');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (35, 'Cody', 'Axcell', 'caxcelly@blinklist.com', 'Male', '72.139.50.44');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (36, 'Irena', 'Matokhnin', 'imatokhninz@blogtalkradio.com', 'Female', '231.167.249.178');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (37, 'Vanya', 'Scandrett', 'vscandrett10@abc.net.au', 'Male', '111.219.18.171');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (38, 'June', 'Calderon', 'jcalderon11@i2i.jp', 'Female', '27.139.126.184');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (39, 'Dorita', 'Ferrari', 'dferrari12@netlog.com', 'Female', '249.54.66.93');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (40, 'Dane', 'Mordanti', 'dmordanti13@tiny.cc', 'Male', '204.133.189.54');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (41, 'Marcella', 'Forber', 'mforber14@nymag.com', 'Female', '72.64.74.35');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (42, 'Bink', 'Baxandall', 'bbaxandall15@seattletimes.com', 'Male', '89.90.152.8');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (43, 'Borden', 'Duncan', 'bduncan16@wix.com', 'Male', '27.95.88.216');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (44, 'Gifford', 'Christophle', 'gchristophle17@4shared.com', 'Male', '176.55.82.215');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (45, 'Nicko', 'Alcide', 'nalcide18@thetimes.co.uk', 'Male', '171.199.220.147');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (46, 'Dilly', 'Hicklingbottom', 'dhicklingbottom19@4shared.com', 'Polygender', '225.201.119.206');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (47, 'Noelani', 'Hupe', 'nhupe1a@altervista.org', 'Female', '159.226.230.231');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (48, 'Renelle', 'Mandre', 'rmandre1b@people.com.cn', 'Female', '81.86.84.2');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (49, 'Sula', 'Sampson', 'ssampson1c@imageshack.us', 'Female', '178.42.161.68');
+insert into testdata (id, first_name, last_name, email, gender, ip_address) values (50, 'Filia', 'Snelling', 'fsnelling1d@dot.gov', 'Genderqueer', '203.242.232.219');
