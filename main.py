098544dc8edce5ef8fac4e1b611fc8c32452fe28
unknown <komronis01@gmail.com>
2023-11-09 13:06:21 +0600

    v1.3 psycopg2 was added

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-# Multiplication table (from 1 to 10) in Python
+#THIS IS NEW VERSION TO CONNECT TO POSTGRESQL
+import psycopg2
 
-num = 112233 # THIS IS UPDATE version 1.2
+conn= psycopg2.connect(host = '127.0.0.1',
+                       database = 'products',
+                       user = 'postgres',
+                       password= '1234567')
+cur = conn.cursor()
+cur.execute('SELECT superhero_name FROM superhero.superhero LIMIT 5')
+usernames = [r[0] for r in cur.fetchall()]
+Found= False
+while not Found:
+    username = input('Введите ваш логин:')
+    if username in usernames:
+        print('Вы есть в списке')
+        Found=True
+    else:
+        print('К сожалению мы не смогли вас найти!')
 
-# To take input from the user
-# num = int(input("Display multiplication table of? "))
-
-# Iterate 10 times from i = 1 to 10
-while num != 0:
-    num-=1
-    
-   
-print('Счёт окончен!')
+conn.commit()
+cur.close()
+conn.close()
